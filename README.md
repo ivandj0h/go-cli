f30ce16505f4893875bb05f33861c341e15801e6
ivandjoh <ivandi.djoh@gmail.com>
2023-01-12 17:35:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Cobra CLI application
+
+This is a simple CLI application built with [Cobra](https://github.com/spf13/cobra-cli)
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/ivandi1980/go-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
